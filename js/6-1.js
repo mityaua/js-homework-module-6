25701fcf441acf7acba33a8f23992ca319b31a46
Dmytro <mityaua@gmail.com>
2020-12-04 11:58:37 +0200

    Добавляет условие первой задачи

---
+++ js/6-1.js
@@ -0,0 +1,14 @@
+// Write code under this line
+const getUserNames = array => array;
+
+// console.log(getUserNames(users));
+
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
